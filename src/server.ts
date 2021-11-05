351372b695c516b9586da6a52812c9f9ca7f22d8
= <=>
2021-11-05 10:45:05 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { json } from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+app.use(json());
+
+app.use("/categories", categoriesRoutes);
+
+app.listen(3333, () => console.log("Server running"));
