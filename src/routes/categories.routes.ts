d5da079e12e82d8a6cad9975e05acb458acd9517
= <=>
2021-11-08 22:52:18 -0300

    part 4 complete

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoriesController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoriesController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const allCategories = categoriesRepository.list();
-
-  return response.json(allCategories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express, { json } from "express";
+import { router } from "./routes";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 app.use(json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server running"));
