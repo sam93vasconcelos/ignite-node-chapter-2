edb8e0077d1e5e2ccceb21ed3fa0dd036ff7ad0e
sam93vasconcelos <samuel93vasconcelos@gmail.com>
2021-11-16 23:20:42 -0300

    Complete part 2

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -2,14 +2,20 @@ import { Request, Response } from "express";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) { }
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    await this.createCategoryUseCase.execute({ name, description });
+    try {
+      await this.createCategoryUseCase.execute({ name, description });
+      return response.status(201).json();
+    } catch (error) {
+      return response.status(500).json({ error: "error" });
+    }
 
-    return response.status(201).json();
+
+
   }
 }
 
